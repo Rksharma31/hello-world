c2ff2e73e9ae912966679eba1314f15839d824ec
Rksharma31 <75877182+Rksharma31@users.noreply.github.com>
2020-12-19 01:54:13 +0530

    Update multiple-3-5.py

--- multiple-3-5.py
+++ multiple-3-5.py
@@ -6,7 +6,7 @@ t=int(input())
 for i in range(t):
     n=int(input())
     n-=1
-    n3=n//3
+    n3=n//6
     n5=n//5
     n15=n//15
     s=((n3*3*(n3+1))//2)+((n5*5*(n5+1))//2)-((n15*15*(n15+1))//2)
