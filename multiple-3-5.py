d71720c6566c208773eba0fbe0f419540aff2aa1
Gaurang Khetan <72882574+gaurang-khetan@users.noreply.github.com>
2020-12-19 01:43:00 +0530

    Euler 1st problem

---
+++ multiple-3-5.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import sys
+s=0
+t=int(input())
+for i in range(t):
+    n=int(input())
+    n-=1
+    n3=n//3
+    n5=n//5
+    n15=n//15
+    s=((n3*3*(n3+1))//2)+((n5*5*(n5+1))//2)-((n15*15*(n15+1))//2)
+    print(s)
+    s=0
